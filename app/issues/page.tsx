1c2785b7d6cb9222d8c319d12f70db89a0e1fec3
Drine Kin <foolishgirl152178@gmail.com>
2024-05-16 20:39:37 +0630

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { Button } from "@radix-ui/themes";
+
 const IssuePage = () => {
-  return <div>IssuePage</div>;
+  return (
+    <div>
+      <Button>New Issues</Button>
+    </div>
+  );
 };
 
 export default IssuePage;
